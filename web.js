f7ca8d5f6067b91b9cc7e07bb72ec1ae6e7b8df7
fatkahawai <ceeb@burning-plains.com>
2013-07-22 04:34:14 +0000

    fix

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 var buffer = fs.readFileSync('index.html');
 
-App.get('/', function(request, response) {
+app.get('/', function(request, response) {
   response.send(buffer.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
